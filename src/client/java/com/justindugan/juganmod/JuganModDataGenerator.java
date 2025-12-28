5af9721950369993db04bfd741a5f0899b881364
jdugan0 <justin.d.dugan@gmail.com>
2025-12-28 15:15:49 -0500

    datagen

---
+++ src/client/java/com/justindugan/juganmod/JuganModDataGenerator.java
@@ -0,0 +1,12 @@
+package com.justindugan.juganmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class JuganModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+	}
+
+}
